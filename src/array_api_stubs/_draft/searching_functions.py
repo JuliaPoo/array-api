689c46d5c32bf2c249763670766d45af4f2aab72
Jules <julia.poo.poo.poo@gmail.com>
2024-06-27 19:40:43 +0800

    WIP: Add top_k spec
    Co-authored-by: kgryte <kgryte@gmail.com>

--- src/array_api_stubs/_draft/searching_functions.py
+++ src/array_api_stubs/_draft/searching_functions.py
@@ -1,4 +1,4 @@
-__all__ = ["argmax", "argmin", "nonzero", "searchsorted", "where"]
+__all__ = ["argmax", "argmin", "nonzero", "searchsorted", "where", "top_k"]
 
 
 from ._types import Optional, Tuple, Literal, array
@@ -156,4 +156,43 @@ def where(condition: array, x1: array, x2: array, /) -> array:
     -------
     out: array
         an array with elements from ``x1`` where ``condition`` is ``True``, and elements from ``x2`` elsewhere. The returned array must have a data type determined by :ref:`type-promotion` rules with the arrays ``x1`` and ``x2``.
+    """
+
+def top_k(
+    x: array,
+    k: int,
+    /,
+    axis: Optional[int] = None,
+    *,
+    largest: bool = True,
+) -> Tuple[array, array]:
+    """
+    Returns the ``k`` largest (or smallest) elements of an input array ``x`` along a specified dimension.
+
+    Parameters
+    ----------
+    x: array
+        input array. Should have a real-valued data type.
+    k: int
+        number of elements to find. Must be a positive integer value.
+    axis: Optional[int]
+        axis along which to search. If ``None``, the function must search the flattened array. Default: ``None``.
+    largest: bool
+        search mode. Must be one of the following modes:
+        -  ``True``: return the ``k`` largest elements.
+        -  ``False``: return the ``k`` smallest elements.
+        Default: ``True``.
+
+    Returns
+    -------
+    out: Tuple[array, array]
+        a tuple ``(values, indices)`` whose
+        - first element must have the field name ``values`` and must be an array containing the ``k`` largest (or smallest) elements of ``x``. The array must have the same data type as ``x``. If ``axis`` is ``None``, the array must be a one-dimensional array having shape ``(k,)``; otherwise, if ``axis`` is an integer value, the array must have the same rank (number of dimensions) and shape as ``x``, except for the axis specified by ``axis`` which must have size ``k``.
+        - second element must have the field name ``indices`` and must be an array containing indices of ``x`` that result in ``values``. The array must have the same shape as ``values`` and must have the default array index data type. If ``axis`` is ``None``, ``indices`` must be the indices of a flattened ``x``.
+
+    Notes
+    -----
+    -   If ``k`` exceeds the number of elements in ``x`` or along the axis specified by ``axis``, behavior is left unspecified and thus implementation-dependent. Conforming implementations may choose, e.g., to raise an exception or return all elements.
+    -   The order of the returned values and indices is left unspecified and thus implementation-dependent. Conforming implementations may return sorted or unsorted values.
+    -   Conforming implementations may support complex numbers; however, inequality comparison of complex numbers is unspecified and thus implementation-dependent (see :ref:`complex-number-ordering`).
     """
